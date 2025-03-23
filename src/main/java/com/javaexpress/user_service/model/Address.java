8dd85dd8f58bfd3e0916ac47dda0621f0c06912c
Highmaster <nzechideraann@gmail.com>
2025-03-23 02:09:37 +0100

    modified the Address class

--- src/main/java/com/javaexpress/user_service/model/Address.java
+++ src/main/java/com/javaexpress/user_service/model/Address.java
@@ -1,5 +1,14 @@
 package com.javaexpress.user_service.model;
 
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@Setter
+@Getter
+@AllArgsConstructor
+@Builder
 public class Address {
 
     private Integer addressId;
