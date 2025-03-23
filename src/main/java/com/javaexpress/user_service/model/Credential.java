05c23fcac6348cd8c72bf89b50efb088b68ee30b
Highmaster <nzechideraann@gmail.com>
2025-03-23 02:17:16 +0100

    modified the credential class

--- src/main/java/com/javaexpress/user_service/model/Credential.java
+++ src/main/java/com/javaexpress/user_service/model/Credential.java
@@ -1,14 +1,37 @@
 package com.javaexpress.user_service.model;
 
+import com.javaexpress.user_service.RoleBasedAuthority;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@Setter
+@Getter
+@AllArgsConstructor
+@Builder
 public class Credential {
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(unique = true, nullable = false)
     private Integer credentialId;
 
+    @Column(unique = true)
     private String username;
     private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name ="role")
+    private RoleBasedAuthority roleBasedAuthority;
 
     private Boolean isEnabled;
     private Boolean isAccountNonExpired;
     private Boolean isAccountNonLocked;
     private Boolean isCredentialsNonExpired;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 }
