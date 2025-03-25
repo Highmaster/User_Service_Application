9573ad71c4b2c358d132a79da4ef7db6f5bddf0b
Highmaster <nzechideraann@gmail.com>
2025-03-25 11:57:38 +0100

    added implement Userservice and the menthods

--- src/main/java/com/javaexpress/user_service/service/UserServiceImpl.java
+++ src/main/java/com/javaexpress/user_service/service/UserServiceImpl.java
@@ -1,9 +1,42 @@
 package com.javaexpress.user_service.service;
 
+import com.javaexpress.user_service.dto.UserDto;
 import com.javaexpress.user_service.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class UserServiceImpl {
+public class UserServiceImpl implements UserService{
     private UserRepository userRepository;
+
+    @Override
+    public UserDto save(UserDto userDto) {
+        return null;
+    }
+
+    @Override
+    public UserDto findById(Integer userId) {
+        return null;
+    }
+
+    @Override
+    public List<UserDto> findALl() {
+        return List.of();
+    }
+
+    @Override
+    public UserDto update(Integer userId, UserDto userDto) {
+        return null;
+    }
+
+    @Override
+    public void daleteById(Integer userId) {
+
+    }
+
+    @Override
+    public UserDto findByUsername(String username) {
+        return null;
+    }
 }
