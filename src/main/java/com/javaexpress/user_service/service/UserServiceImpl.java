77449259859c2731445e0064239c56b02c53d995
Highmaster <nzechideraann@gmail.com>
2025-03-25 11:50:46 +0100

    added the UserServiceImpl class

---
+++ src/main/java/com/javaexpress/user_service/service/UserServiceImpl.java
@@ -0,0 +1,9 @@
+package com.javaexpress.user_service.service;
+
+import com.javaexpress.user_service.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl {
+    private UserRepository userRepository;
+}
