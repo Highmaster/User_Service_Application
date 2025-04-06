8bafdb536727d2ff6baa884bfb3fdf0f66aa630b
Highmaster <nzechideraann@gmail.com>
2025-04-06 09:20:59 +0100

    added  PostMapping method

--- src/main/java/com/javaexpress/controller/UserController.java
+++ src/main/java/com/javaexpress/controller/UserController.java
@@ -1,4 +1,28 @@
 package com.javaexpress.controller;
 
+import com.javaexpress.dto.UserDto;
+import com.javaexpress.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping(value ="/api/users")
+@Slf4j
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserDto> save(@RequestBody UserDto userDto) {
+        UserDto dbUser = userService.save(userDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(dbUser);
+    }
 }
