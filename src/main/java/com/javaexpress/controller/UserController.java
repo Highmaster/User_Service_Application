573b4c8b02b637f84be536cf40cf74d239080bc2
Highmaster <nzechideraann@gmail.com>
2025-04-06 09:25:50 +0100

    added the GetMapping method

--- src/main/java/com/javaexpress/controller/UserController.java
+++ src/main/java/com/javaexpress/controller/UserController.java
@@ -6,10 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -24,5 +21,11 @@ public class UserController {
     public ResponseEntity<UserDto> save(@RequestBody UserDto userDto) {
         UserDto dbUser = userService.save(userDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(dbUser);
+    }
+
+    @GetMapping("{userId}")
+    public ResponseEntity<UserDto> findById(@PathVariable Integer userId) {
+        log.info("UserController :: fetch user details");
+        return ResponseEntity.ok(userService.findById(userId));
     }
 }
