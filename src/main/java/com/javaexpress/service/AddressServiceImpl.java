e51585ba1e30942a10f2b81dbdc6de2f7bc8db7d
Highmaster <nzechideraann@gmail.com>
2025-04-14 13:24:00 +0100

    Added a method to retrieve a user from the db, maps AddressDto to an Address entity, sets the user, saves the address, and returns the saved data asa a DTO

--- src/main/java/com/javaexpress/service/AddressServiceImpl.java
+++ src/main/java/com/javaexpress/service/AddressServiceImpl.java
@@ -1,9 +1,11 @@
 package com.javaexpress.service;
 
 import com.javaexpress.dto.AddressDto;
+import com.javaexpress.helper.AddressMappingHelper;
 import com.javaexpress.model.Address;
 import com.javaexpress.model.User;
 import com.javaexpress.repository.AddressRepository;
+import com.javaexpress.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,8 +18,20 @@ public class AddressServiceImpl implements AddressService{
     @Autowired
     private AddressRepository addressRepository;
 
+    private UserRepository userRepository;
+
     @Override
     public AddressDto save(AddressDto addressDto) {
+
+        Integer userId = addressDto.getUserDto().getUserId();
+        User dbUser =  userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User Not Found"));
+        Address address = AddressMappingHelper.map(addressDto);
+        address.setUser(dbUser);
+        Address dbaddress = addressRepository.save(address);
+
+        return AddressMappingHelper.map(dbaddress);
+
         return null;
     }
 
