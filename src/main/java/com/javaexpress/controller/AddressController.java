9b63f3ca00bf311765673b6cbbdd740835eefdff
Highmaster <nzechideraann@gmail.com>
2025-04-14 13:27:38 +0100

    added the AdressController class wit annotations

---
+++ src/main/java/com/javaexpress/controller/AddressController.java
@@ -0,0 +1,15 @@
+package com.javaexpress.controller;
+
+
+import com.javaexpress.service.AddressService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = {"/api/address"})
+@Slf4j
+public class AddressController {
+
+    private AddressService addressService;
+}
