4a6b5322c25c888c04fb84d1e3702e7d1398bc1d
Highmaster <nzechideraann@gmail.com>
2025-04-14 13:31:05 +0100

    added the postmapping method

--- src/main/java/com/javaexpress/controller/AddressController.java
+++ src/main/java/com/javaexpress/controller/AddressController.java
@@ -1,8 +1,10 @@
 package com.javaexpress.controller;
 
 
+import com.javaexpress.dto.AddressDto;
 import com.javaexpress.service.AddressService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class AddressController {
 
     private AddressService addressService;
+
+    public AddressDto save(@RequestBody AddressDto addressDto) {
+        return addressService.save(addressDto);
+    }
 }
