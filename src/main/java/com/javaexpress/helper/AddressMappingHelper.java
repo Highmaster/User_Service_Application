16c1c971c03e2c595c6f643a3d6a336d16824a5c
Highmaster <nzechideraann@gmail.com>
2025-04-14 12:33:36 +0100

    added the AddressMappingHelper class

---
+++ src/main/java/com/javaexpress/helper/AddressMappingHelper.java
@@ -0,0 +1,16 @@
+package com.javaexpress.helper;
+
+import com.javaexpress.dto.AddressDto;
+import com.javaexpress.model.Address;
+
+public interface AddressMappingHelper {
+
+    public static AddressDto map(Address address) {
+        return  null;
+    }
+
+    public static Address map(AddressDto addressDto) {
+        Address add
+        return null;
+    }
+}
