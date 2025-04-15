f5fd2865f80d99a3131661f2f6214e906680e440
Highmaster <nzechideraann@gmail.com>
2025-04-15 12:28:13 +0100

    modified the AddressController

--- src/main/java/com/javaexpress/controller/AddressController.java
+++ src/main/java/com/javaexpress/controller/AddressController.java
@@ -4,18 +4,23 @@ package com.javaexpress.controller;
 import com.javaexpress.dto.AddressDto;
 import com.javaexpress.service.AddressService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = {"/api/address"})
 @Slf4j
 public class AddressController {
 
+    @Autowired
     private AddressService addressService;
 
+    @PostMapping
     public AddressDto save(@RequestBody AddressDto addressDto) {
         return addressService.save(addressDto);
+    }
+
+    public AddressDto findById(@PathVariable Integer addressId) {
+        return addressService.findById(addressId);
     }
 }
